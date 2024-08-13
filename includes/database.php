271fe6d865dc3d240d4461c403933db38f59b364
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-08-12 20:21:45 -0600

    Conifigurando ENV y Database; agregando primeras rutas del proyecto y Login Controller

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', '', '', '');
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
